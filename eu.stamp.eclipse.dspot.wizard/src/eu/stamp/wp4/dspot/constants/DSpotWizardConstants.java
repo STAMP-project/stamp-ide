5ba18ec753b43180997681ea0cf10782b2d86247
Ricardo Jose Tejada Garcia <ricardo-jose.tejadagarcia@atos.net>
2018-05-23 12:44:10 +0200

    "tooltips added"

--- eu.stamp.eclipse.dspot.wizard/src/eu/stamp/wp4/dspot/constants/DSpotWizardConstants.java
+++ eu.stamp.eclipse.dspot.wizard/src/eu/stamp/wp4/dspot/constants/DSpotWizardConstants.java
@@ -12,7 +12,7 @@
  *******************************************************************************/
 package eu.stamp.wp4.dspot.constants;
 
-public class DSpotWizardConstants {
+public abstract class DSpotWizardConstants {
 	public final static String PLUGIN_NAME = "eu.stamp.eclipse.dspot.wizard";
 	public final static String DSPOT_JAR = "platform:/plugin/eu.stamp.eclipse.dspot.wizard/lib/dspot-1.1.1-SNAPSHOT-jar-with-dependencies.jar";
 	public final static String MAVEN_NATURE = "org.eclipse.m2e.core.maven2Nature";
