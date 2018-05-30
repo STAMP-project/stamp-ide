eef457f540cd452d93ff19ddb7ee0bd8a5bcb40c
Ricardo Jose Tejada Garcia <ricardo-jose.tejadagarcia@atos.net>
2018-05-30 16:38:28 +0200

    "Created Descartes view"

--- eu.stamp.eclipse.descartes.wizard/src/eu/stamp/wp4/descartes/view/DescartesView.java
+++ eu.stamp.eclipse.descartes.wizard/src/eu/stamp/wp4/descartes/view/DescartesView.java
@@ -7,19 +7,14 @@ import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.part.ViewPart;
-
-import eu.stamp.wp4.descartes.wizard.utils.DescartesHtmlManager;
 
 public class DescartesView extends ViewPart {
 	
 	@Inject IWorkbench workbench;
 
 	public static final String ID = "eu.stamp.wp4.descartes.view.DescartesView";
-	
-	private String[] urls;
 	
 	private Composite parent;
 	
@@ -31,7 +26,6 @@ public class DescartesView extends ViewPart {
 	public void setFocus() {}
     
 	public void setUrls(String[] urls) { 
-		this.urls = urls;
 		if(urls != null) {
 			GridLayoutFactory.fillDefaults().applyTo(parent);
 	        Browser[] browsers = new Browser[urls.length];
