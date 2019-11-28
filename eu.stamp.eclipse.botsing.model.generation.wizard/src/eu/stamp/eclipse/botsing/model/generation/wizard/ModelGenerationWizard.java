6bdd1952eb30b871d2999af3fa6ac38a87823a1e
Ricardo Tejada <ricardo.tejada@atos.net>
2019-11-28 16:36:06 +0100

    model generation wizard checks configuration

--- eu.stamp.eclipse.botsing.model.generation.wizard/src/eu/stamp/eclipse/botsing/model/generation/wizard/ModelGenerationWizard.java
+++ eu.stamp.eclipse.botsing.model.generation.wizard/src/eu/stamp/eclipse/botsing/model/generation/wizard/ModelGenerationWizard.java
@@ -18,12 +18,16 @@ public class ModelGenerationWizard extends Wizard {
 	private final IJavaProject project;
 	
 	public ModelGenerationWizard(IJavaProject project) {
+		this(project,"");
+	}
+	
+	public ModelGenerationWizard(IJavaProject project, String projectClassPath) {
 		
 	super();
 		
 	map = new HashMap<String,String>();
 	
-	map.put(ModelGenerationLaunchConstants.PROJECT_CLASS_PATH,"");
+	map.put(ModelGenerationLaunchConstants.PROJECT_CLASS_PATH,projectClassPath);
 	map.put(ModelGenerationLaunchConstants.PROJECT_PREFIX,"");
 	map.put(ModelGenerationLaunchConstants.OUT_DIR,ModelGenerationLaunchConstants.OUT_DIR_DEFAULT);
 	
@@ -42,10 +46,24 @@ public class ModelGenerationWizard extends Wizard {
 	}
 	/**
 	 *  this wizard provide the job to other plugins in order to execute it in the
-	 *  right moment, it dosn't execute the job
+	 *  right moment, it dosn't execute the job, the job will be null if the classpath or project prefix is not set
 	 */
 	@Override
 	public boolean performFinish() {
+		
+		// check
+		String target = map.get(ModelGenerationLaunchConstants.PROJECT_CLASS_PATH);
+		if(target == null || target.replaceAll(" ","").isEmpty()) {
+			job = null;
+			return true;
+		}
+		target = map.get(ModelGenerationLaunchConstants.PROJECT_PREFIX);
+		if(target == null || target.replaceAll(" ","").isEmpty()) {
+			job = null;
+			return true;
+		}
+		
+		// crete job
 		StringBuilder builder = new StringBuilder();
 		for(String key : map.keySet())if(!map.get(key).isEmpty())
 			builder.append(key).append(' ').append(map.get(key)).append(' ');
