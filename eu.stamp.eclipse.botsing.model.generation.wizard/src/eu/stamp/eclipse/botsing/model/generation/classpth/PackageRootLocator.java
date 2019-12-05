e2d313a9ef1b73e3abfd4a6679a3b97b443f33be
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2019-12-05 18:06:46 +0100

    Added copyright notice

--- eu.stamp.eclipse.botsing.model.generation.wizard/src/eu/stamp/eclipse/botsing/model/generation/classpth/PackageRootLocator.java
+++ eu.stamp.eclipse.botsing.model.generation.wizard/src/eu/stamp/eclipse/botsing/model/generation/classpth/PackageRootLocator.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Atos
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * Contributors:
+ * 	Ricardo Jose Tejada Garcia (Atos) - main developer
+ * 	Jesús Gorroñogoitia (Atos) - architect
+ * Initially developed in the context of STAMP EU project https://www.stamp-project.eu
+ *******************************************************************************/
 package eu.stamp.eclipse.botsing.model.generation.classpth;
 
 import java.io.File;
--- eu.stamp.eclipse.botsing.plugin/src/eu/stamp/eclipse/botsing/wizard/handler/BotsingWizardHandler.java
+++ eu.stamp.eclipse.botsing.plugin/src/eu/stamp/eclipse/botsing/wizard/handler/BotsingWizardHandler.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2018 Atos
+ * Copyright (c) 2019 Atos
  * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
  *
  * Contributors:
- * Ricardo José Tejada García (Atos) - main developer
- * Jesús Gorroñogoitia (Atos) - architect
+ * 	Ricardo Jose Tejada Garcia (Atos) - main developer
+ * 	Jesús Gorroñogoitia (Atos) - architect
  * Initially developed in the context of STAMP EU project https://www.stamp-project.eu
  *******************************************************************************/
 package eu.stamp.eclipse.botsing.wizard.handler;
